630ca0d076a92e67f467e4d77904b95a3f29bf05
Yihan Li <Liyihan11unique@outlook.com>
2026-03-31 14:00:12 +0800

    verison 3.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 27
         targetSdk = 35
         versionCode = 1
-        versionName = "3.0.1"
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
